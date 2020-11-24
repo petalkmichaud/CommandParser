41066628dcac91bf3748a6e61a49ceada98ff767
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-11-24 12:23:52 -0800

    Update CommandParser.java

--- CommandParser.java
+++ CommandParser.java
@@ -3,11 +3,16 @@ package cs350f20project.controller.cli.parser;
 
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
+import cs350f20project.controller.cli.TrackLocator;
 import cs350f20project.controller.command.A_Command;
 import cs350f20project.controller.command.PointLocator;
 import cs350f20project.controller.command.behavioral.CommandBehavioralBrake;
 import cs350f20project.controller.command.behavioral.CommandBehavioralSelectBridge;
+import cs350f20project.controller.command.creational.CommandCreatePowerCatenary;
+import cs350f20project.controller.command.creational.CommandCreatePowerPole;
+import cs350f20project.controller.command.creational.CommandCreatePowerStation;
 import cs350f20project.controller.command.creational.CommandCreateTrackCurve;
 import cs350f20project.controller.command.creational.CommandCreateTrackEnd;
 import cs350f20project.controller.command.meta.CommandMetaDoExit;
@@ -20,11 +25,13 @@ import cs350f20project.datatype.Longitude;
 
 public class CommandParser {
  
-  
+  private LinkedList<String> idSubstations=new LinkedList<>();
   private Angle angle;
   private CoordinatesDelta coordinates_delta;
   private CoordinatesDelta coordinates_delta3=new CoordinatesDelta(1,1);
   private String id1="sss";
+  private String idn ="ssss";
+  private String id2="zz";
   public static String id="zzz";
   private int integer;
   private Latitude latitude=new Latitude(1);
@@ -35,10 +42,13 @@ public class CommandParser {
   private CoordinatesWorld coordinates_world=new CoordinatesWorld(latitude, longitude);
   private double number=1;
   private double real;
+  private TrackLocator START;
+  private TrackLocator STOP;
   private String commandText;
   private MyParserHelper parserHelper;
   private CommandParserTokenManager tm;
   private PointLocator locater=new PointLocator(coordinates_world, coordinates_delta1, coordinates_delta2);
+  private LinkedList<String> idpoles=new LinkedList();
   
 	public CommandParser(MyParserHelper parserHelper, String commandText){
 		this.parserHelper=parserHelper;
@@ -48,6 +58,7 @@ public class CommandParser {
 		this.tm=tm;
 	}
 	public void parse() {
+    //need to go back and add $ 
 	String[] x=("Do Break "+id).split(" ");        
 	//#44
 	 String[] command44=("CREATE TRACK "+id+"REFERENCE "+coordinates_world+"DELTA START "+coordinates_delta1+"END "+coordinates_delta2).split(" ");
@@ -60,6 +71,16 @@ public class CommandParser {
 	//6
 	    String[] command6=("DO SELECT DRAWBRIDGE "+id+"POSITION UP").split(" ");
 	    String[] command61=("DO SELECT DRAWBRIDGE "+id+"POSITION DOWN").split(" ");
+	//22 
+	    String[] command22=("CREATE POWER CATENARY "+id1+"WITH POLES "+idn+"+").split(" ");
+	//23 
+	    String[] command23=("CREATE POWER POLE "+id1+"ON TRACK "+id2+"DISTANCE "+number+"FROM "+"START").split(" ");
+	    String[] command232=("CREATE POWER POLE "+id1+"ON TRACK "+id2+"DISTANCE "+number+"FROM "+"END").split(" ");
+	//24
+	    String[] command24=("CREATE POWER STATION "+id1+"REFERENCE "+coordinates_world+"DELTA "+coordinates_delta+"WITH SUBSTATION").split(" ");
+	    String[] command242=("CREATE POWER STATION "+id1+"REFERENCE "+id2+"DELTA "+coordinates_delta+"WITH SUBSTATION").split(" ");
+	    String[] command243=("CREATE POWER STATION "+id1+"REFERENCE "+coordinates_world+"DELTA "+coordinates_delta+"WITH SUBSTATIONS").split(" ");
+	    String[] command244=("CREATE POWER STATION "+id1+"REFERENCE "+id2+"DELTA "+coordinates_delta+"WITH SUBSTATIONS").split(" ");
 	    if(this.commandText.equals("COMMIT")) {
 	    	A_Command command=new CommandStructuralCommit();
 	    	this.parserHelper.getActionProcessor().schedule(command);
@@ -95,7 +116,22 @@ public class CommandParser {
 	    	A_Command command=new CommandBehavioralSelectBridge(id, true);
 	    	this.parserHelper.getActionProcessor().schedule(command);
 	    }
-	    
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command22))) {
+	    	A_Command command=new CommandCreatePowerCatenary(id, idpoles);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command23))) {
+	    	A_Command command=new CommandCreatePowerPole(id, START);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command232))) {
+	    	A_Command command=new CommandCreatePowerPole(id, STOP);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command24)) || this.commandText.equalsIgnoreCase(Arrays.toString(command243))) {
+	    	A_Command command=new CommandCreatePowerStation(id, coordinates_world, coordinates_delta1, idSubstations);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
 	}
 	public String getID() {
 		return this.id;
@@ -112,4 +148,14 @@ public class CommandParser {
 	public double getNumber() {
 		return this.number;
 	}
+	public LinkedList getpoles() {
+		return this.idpoles;
+	}
+	public String getId1() {
+		return this.id1;
+	}
+	public String getIdn() {
+		return idn;
+	}
 }
+
