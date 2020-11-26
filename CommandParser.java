27638d4fc5ae89813c1b6deebdcaf5170615aacb
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2020-11-26 13:25:43 -0800

    Update CommandParser.java

--- CommandParser.java
+++ CommandParser.java
@@ -2,6 +2,7 @@ package cs350f20project.controller.cli.parser;
 
 
 
+import java.awt.List;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -13,8 +14,12 @@ import cs350f20project.controller.command.behavioral.CommandBehavioralSelectBrid
 import cs350f20project.controller.command.creational.CommandCreatePowerCatenary;
 import cs350f20project.controller.command.creational.CommandCreatePowerPole;
 import cs350f20project.controller.command.creational.CommandCreatePowerStation;
+import cs350f20project.controller.command.creational.CommandCreateTrackBridgeFixed;
+import cs350f20project.controller.command.creational.CommandCreateTrackCrossing;
+import cs350f20project.controller.command.creational.CommandCreateTrackCrossover;
 import cs350f20project.controller.command.creational.CommandCreateTrackCurve;
 import cs350f20project.controller.command.creational.CommandCreateTrackEnd;
+import cs350f20project.controller.command.creational.CommandCreateTrackLayout;
 import cs350f20project.controller.command.meta.CommandMetaDoExit;
 import cs350f20project.controller.command.structural.CommandStructuralCommit;
 import cs350f20project.datatype.Angle;
@@ -39,6 +44,8 @@ public class CommandParser {
   private Longitude longitude=new Longitude(1);
   private CoordinatesDelta coordinates_delta1=new CoordinatesDelta(1,1);
   private CoordinatesDelta coordinates_delta2=new CoordinatesDelta(1,1);
+
+  private CoordinatesDelta coordinates_delta4=new CoordinatesDelta(1,1);
   private CoordinatesWorld coordinates_world=new CoordinatesWorld(latitude, longitude);
   private double number=1;
   private double real;
@@ -49,7 +56,7 @@ public class CommandParser {
   private CommandParserTokenManager tm;
   private PointLocator locater=new PointLocator(coordinates_world, coordinates_delta1, coordinates_delta2);
   private LinkedList<String> idpoles=new LinkedList();
-  
+  private LinkedList<String> trackIDs=new LinkedList<String>();
 	public CommandParser(MyParserHelper parserHelper, String commandText){
 		this.parserHelper=parserHelper;
 		this.commandText=commandText;
@@ -59,14 +66,20 @@ public class CommandParser {
 	}
 	public void parse() {
     //need to go back and add $ 
-	String[] x=("Do Break "+id).split(" ");        
+	String[] x=("Do Break "+id).split(" ");  
+	//40
+	String[] command40=("CREATE TRACK BRIDGE "+id1+"REFERENCE "+coordinates_world+" | '$' "+id2+"DELTA START "+coordinates_delta1+"END "+coordinates_delta2).split(" ");
+	//41
+	String[] command41=("CREATE TRACK CROSSING "+id1+"REFERENCE "+coordinates_world+" | '$' "+id2+"DELTA START "+coordinates_delta1+"END "+coordinates_delta2).split(" ");
+	//42
+	String[] command42=("CREATE TRACK CROSSOVER "+id1+"REFERENCE "+coordinates_world+" | '$' "+id2+"DELTA START "+coordinates_delta1+"END "+coordinates_delta2+"START "+coordinates_delta3+"END "+coordinates_delta4).split(" ");
 	//#44
-	 String[] command44=("CREATE TRACK "+id1+"REFERENCE "+coordinates_world+"'$' "+id2+"DELTA START "+coordinates_delta1+"END "+coordinates_delta2).split(" ");
+	 String[] command44=("CREATE TRACK "+id1+" REFERENCE "+coordinates_world+"'$' "+id2+"DELTA START "+coordinates_delta1+"END "+coordinates_delta2).split(" ");
 
 	//43    
 	    String[] command43=("CREATE TRACK CURVE "+id1+"REFERENCE "+coordinates_world+"'$' "+id2+"DELTA START "+coordinates_delta1+"END"+coordinates_delta2+"DISTANCE ORIGIN "+number+" | "+"ORIGIN "+coordinates_delta3).split(" ");
-
-
+    //45
+        String[] command45=("CREATE TRACK LAYOUT "+id1+"WITH TRACKS "+idn+"+").split(" ");
 	//22 
 	    String[] command22=("CREATE POWER CATENARY "+id1+"WITH POLES "+idn+"+").split(" ");
 	//23 
@@ -101,7 +114,6 @@ public class CommandParser {
 	    	this.parserHelper.getActionProcessor().schedule(command);
 	    }
 
-
 	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command22))) {
 	    	A_Command command=new CommandCreatePowerCatenary(id, idpoles);
 	    	this.parserHelper.getActionProcessor().schedule(command);
@@ -119,6 +131,23 @@ public class CommandParser {
 	    	A_Command command=new CommandCreatePowerStation(id, coordinates_world, coordinates_delta, idSubstations);
 	    	this.parserHelper.getActionProcessor().schedule(command);
 	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command42))) {
+	    	A_Command command=new CommandCreateTrackCrossover(id, coordinates_world, coordinates_delta1, coordinates_delta2, coordinates_delta3, coordinates_delta4);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command41))) {
+	    	A_Command command=new CommandCreateTrackCrossing(id, locater);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command40))) {
+	    	A_Command command=new CommandCreateTrackBridgeFixed(id, locater);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	    else if(this.commandText.equalsIgnoreCase(Arrays.toString(command45))) {
+	    	A_Command command=new CommandCreateTrackLayout(id,trackIDs);
+	    	this.parserHelper.getActionProcessor().schedule(command);
+	    }
+	        
 	}
 	public String getID() {
 		return this.id;
